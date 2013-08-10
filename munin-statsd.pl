95783ee1ac9f0eb7d9c12ce555c2c23a24bd684c
Carlos Peñas <carlos.penas@the-cocktail.com>
2013-08-10 20:25:51 +0200

    whitespace

--- munin-statsd.pl
+++ munin-statsd.pl
@@ -2,27 +2,27 @@
 
 #
 # munin-statsd.pl
-# 
+#
 # A Munin-Node to StatsD bridge
 #
 #    Author:: Brian Staszewski (<brian.staszewski@tech-corps.com>)
 # Copyright:: Copyright (c) 2012 Tech-Corps, Inc.
 #   License:: GNU General Public License version 2 or later
-# 
+#
 # This program and entire repository is free software; you can
-# redistribute it and/or modify it under the terms of the GNU 
-# General Public License as published by the Free Software 
+# redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software
 # Foundation; either version 2 of the License, or any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 
 use warnings;
 use strict;
@@ -36,7 +36,6 @@ my $statsdhost		= "statsd.company.com";
 my $statsdport		= 8125;
 my $muninhost		= shift || "localhost";
 my $muninport		= 4949;
-
 
 $|++;
 
